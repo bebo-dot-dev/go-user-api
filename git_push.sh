4a2a85265c95b420e08e39e55679e2fdec3dd1c3
Joe <joe@jjssoftware.co.uk>
2021-02-20 16:11:30 +0000

    force push

--- git_push.sh
+++ git_push.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 git config --global user.email "joe@jjssoftware.co.uk"
 git config --global user.name "Joe"
+git fetch --all
 git add .
 git commit -m "automated codegen commit"
-git push
+git pull --rebase
+git push --force
